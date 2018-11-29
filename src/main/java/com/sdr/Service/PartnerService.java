597b30f1d6eff224fa0a91da570bcdbbb7404d4d
Spencer Ragen <sragen@gmail.com>
2018-11-29 17:01:23 -0500

    Adjusted getPartner() to match the new definition Changed the service to use the mysql layer instead of static data

--- src/main/java/com/sdr/Service/PartnerService.java
+++ src/main/java/com/sdr/Service/PartnerService.java
@@ -1,21 +1,26 @@
 package com.sdr.Service;
 
 import com.sdr.DAO.PartnerDaoInterface;
+
 import com.sdr.Entity.Campaign;
 import com.sdr.Entity.Partner;
+
 import com.sdr.Exception.NoActiveCampaignException;
 import com.sdr.Exception.NoSuchPartnerException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLException;
+
 @Service
 public class PartnerService {
     @Autowired
-    @Qualifier("static")
+    @Qualifier("mysql")
     private PartnerDaoInterface partnerDao;
 
-    public Partner getPartner(String partnerId) throws NoSuchPartnerException {
+    public Partner getPartner(String partnerId) throws NoSuchPartnerException, SQLException {
         return this.partnerDao.getPartner(partnerId);
     }
 
