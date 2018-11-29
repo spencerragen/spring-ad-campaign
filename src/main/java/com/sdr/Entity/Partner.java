16aa712a7704797293e484065b57c742b9d23278
Spencer Ragen <sragen@gmail.com>
2018-11-29 17:00:45 -0500

    Added an api key field to Partner entity

--- src/main/java/com/sdr/Entity/Partner.java
+++ src/main/java/com/sdr/Entity/Partner.java
@@ -4,10 +4,23 @@ import java.util.Collection;
 
 public class Partner {
     private String partnerId;
+    private String apiKey;
+
     private Collection<Campaign> campaigns;
 
     public Partner(String partnerId) {
         this.partnerId = partnerId;
+    }
+
+    public Partner(String partnerId, String apiKey) {
+        this.partnerId = partnerId;
+        this.apiKey = apiKey;
+    }
+
+    public Partner(String partnerId, String apiKey, Collection<Campaign> campaigns) {
+        this.partnerId = partnerId;
+        this.apiKey = apiKey;
+        this.campaigns = campaigns;
     }
 
     public Partner(String partnerId, Collection<Campaign> campaigns) {
@@ -21,6 +34,14 @@ public class Partner {
 
     public void setPartnerId(String partnerId) {
         this.partnerId = partnerId;
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
     }
 
     public Collection<Campaign> getCampaigns() {
