7b8b31b17884d81db6fd113ad8e298e3c2a65a41
Spencer Ragen <sragen@gmail.com>
2018-11-29 16:57:20 -0500

    Changed definition of getPartner() to also throw SQLException

--- src/main/java/com/sdr/DAO/PartnerDaoInterface.java
+++ src/main/java/com/sdr/DAO/PartnerDaoInterface.java
@@ -6,7 +6,10 @@ import com.sdr.Entity.Partner;
 import com.sdr.Exception.NoActiveCampaignException;
 import com.sdr.Exception.NoSuchPartnerException;
 
+import java.sql.SQLException;
+import java.util.Calendar;
+
 public interface PartnerDaoInterface {
-    Partner getPartner(String partnerId) throws NoSuchPartnerException;
+    Partner getPartner(String partnerId) throws NoSuchPartnerException, SQLException;
     Campaign getActiveCampaign(String partnerId) throws NoSuchPartnerException, NoActiveCampaignException;
 }
